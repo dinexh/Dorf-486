e620c2a80844a518e97a957f286fabe0b3df1438
Dinesh korukonda <dineshkorukonda05@gmail.com>
2025-02-18 13:37:24 +0530

    added new next app

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
-export default function Home() {
-  return (
+export default function Home(){
+  return(
     <div className="home-component">
-      <div className="home-component-in">
-        
-      </div>
+      <h1>Smart Village Revolution</h1>
     </div>
-  );
+  )
 }
